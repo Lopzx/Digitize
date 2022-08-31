0e41abf43133fd4be0a28fe977475c278209a332
AurellGE <aurelliagita01@gmail.com>
2022-08-31 12:51:29 +0700

    [fix]tailwind.config.js: add screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
     ],
 
     theme: {
+        screens: {
+            mobile: "320px",
+            // => @media (min-width: 320px) { ... }
+
+            tablet: "768px",
+            // => @media (min-width: 768px) { ... }
+
+            laptop: "1024px",
+            // => @media (min-width: 1040px) { ... }
+
+            desktop: "1280px",
+            // => @media (min-width: 1280px) { ... }
+        },
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
