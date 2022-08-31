07d903a8d6ae51679efe4092a0219d7babc359a1
vinvintjg <vincent88tjong@gmail.com>
2022-08-31 21:37:09 +0700

    final2

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::redirect('/', '/home');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
